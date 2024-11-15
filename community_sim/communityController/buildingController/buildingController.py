Added script to rename osm models such that they don't all show up as building 1 in alfalfa. Added normalizing the building controllers. Fixed a bug where the indoor air temperature from alfalfa did not update the value used by the building controllers.

community_sim/communityController/buildingController/buildingController.py
@@ -28,8 +28,8 @@ class BuildingController:
             id: (str) id of the controller's building
             testCase: (str) name of test case being run, defaults to MPC
         """
-        self.actuatorValues = {'heatingSetpoint': 0, 'coolingSetpoint': 0, 'battery': 0}
-        self.sensorValues = {'indoorAirTemp': 0, 'batterySOC': 5}
+        self.actuatorValues = {'heatingSetpoint': 18, 'coolingSetpoint': 24, 'battery': 0}
+        self.sensorValues = {'indoorAirTemp': 21, 'batterySOC': 8.4}
         self.buildingID = id
         self.devices = devices
 
@@ -44,7 +44,7 @@ class BuildingController:
         self.cl_system = None
 
         # Load building specific parameters
-        self.setpointInfo = {"heatSP": 18.88888888888889, "coolSP": 24.444444444444443, "deadband": 4.444444444444445}      # Default value, should be commented out
+        self.setpointInfo = {"heatSP": 18.88888888888889, "coolSP": 24.444444444444443, "deadband": 1}      # Default value, should be commented out
         # Temporary, needs to be fixed later
         self.setpointSchedule = pd.read_csv(os.path.join(self.dirName, '../../setpointSchedule.csv')
                                             , header=None).to_numpy()[np.newaxis, :]
@@ -88,8 +88,8 @@ class BuildingController:
                             'stored': torch.tensor(np.array([self.sensorValues['batterySOC']])[np.newaxis,:,np.newaxis], dtype=torch.float32),
                             'batRef': torch.tensor(self.socSchedule.take(range(currentMinutes, currentMinutes+self.nsteps), axis=0, mode='wrap')[np.newaxis,:], dtype=torch.float32),
                             'batMax': torch.tensor(np.ones((1,self.nsteps+1,1))*8.0, dtype=torch.float32)}
-        for key, value in self.horizonData.items():
-            print(key, value.shape)
+        
+        self.horizonData = self.norm.norm(self.horizonData, keys=['y', 'y', 'y', 'y', 'd', 'leave', 'p', 'leave', 'leave', 'leave'])
 
     def PushControlSignals(self):
         """
@@ -175,8 +175,8 @@ class BuildingController:
 
         # ----- Make sure you run prepareRun.py first -----
         run = 'latestRun'
-        # run = 'bat_AB_test_1'
-        run = 'bat_AB_test_2'
+        # run = 'bat_AB_test_2'
+        # run = 'bat_AB_test_3'
         # run = 'alf_AllBuildings_1stPass'
         # --------------------------------------------------
 
@@ -185,8 +185,8 @@ class BuildingController:
 
         manager = RunManager(run, saveDir=f'{filePath}/deployModels')
         manager.LoadRunJson(run)
-        norm = Normalizer()
-        norm.load(f"{manager.runPath}norm/{self.buildingID}/")
+        self.norm = Normalizer()
+        self.norm.load(f"{manager.runPath}norm/{self.buildingID}/")
 
         for key in manager.models.keys():
             if key.find('buildingThermal') != -1:
@@ -242,7 +242,7 @@ class BuildingController:
                                          d_idx=initParams['d_idx'],
                                          manager=manager,
                                          name=controllerModelName,
-                                         norm=norm,
+                                         norm=self.norm,
                                          thermalModel=buildingThermal.model,
                                          classifier=classifier.model,
                                          device=device,

community_sim/communityController/buildingController/thermal_node_model/modelConstructor.py
@@ -581,6 +581,8 @@ class ControllerSystem(NeuromancerModel):
         self.hsizes = manager.models[name]['hsizes']
         self.n_samples = manager.models[name]['train_params']['n_samples']
 
+        self.batSize = 16.4
+
         self.thermalModel = thermalModel
         self.classifier = classifier
 
@@ -607,11 +609,11 @@ class ControllerSystem(NeuromancerModel):
         outsize=self.nu,
         hsizes=self.hsizes,
         nonlin=nn.GELU,
-        min=-3.8,
-        max=3.8)
+        min=-9.6,
+        max=9.6)
         batPolicy = Node(batNet, ['cost', 'dr', 'batRef', 'batMax'], ['u_bat'], name='batPolicy')
 
-        batModel = Node(BatteryModel(eff=0.95, capacity=10, nx=1, nu=1), ['stored', 'u_bat'], ['stored'], name='batModel')
+        batModel = Node(BatteryModel(eff=0.95, capacity=self.batSize, nx=1, nu=1), ['stored', 'u_bat'], ['stored'], name='batModel')
 
         # Freeze thermal model
         for p in self.thermalModel.nodes[0].parameters():
@@ -649,7 +651,6 @@ class ControllerSystem(NeuromancerModel):
 
         # objectives
         u_tot = u + u_bat
-        action_loss = u_tot.minimize(weight=self.weights['action_loss'], name='action_loss')
 
         dr_loss = self.weights['dr_loss'] * (((u_tot) * dr) == torch.tensor(np.zeros((self.batch_size, self.nsteps, 1)), device=self.device))
         dr_loss.name = 'dr_loss'
@@ -669,7 +670,7 @@ class ControllerSystem(NeuromancerModel):
         # bat_life_upper_bound_penalty.name = 'bat_life_max'
 
         # list of constraints and objectives
-        objectives = [action_loss, dr_loss, cost_loss]
+        objectives = [dr_loss, cost_loss]
         constraints = [state_lower_bound_penalty, state_upper_bound_penalty, bat_life_lower_bound_penalty]
 
         # Problem construction
@@ -746,9 +747,10 @@ class ControllerSystem(NeuromancerModel):
                           dtype=torch.float32, device=self.device).reshape(1,1,self.nx)
         dr = torch.tensor(psl.signals.step(nsteps_test+1, 1, min=0, max=1, randsteps=5, rng=self.rng), dtype=torch.float32).reshape(1, nsteps_test+1, 1)
         dr = torch.tensor(np.zeros((1, nsteps_test+1, 1)), dtype=torch.float32)
-        stored0 = torch.tensor(self.rng.uniform(0,10,[1,1,1]), dtype=torch.float32, device=self.device)
-        batRef = torch.tensor(pd.read_csv('socSchedule.csv', header=None).to_numpy(), dtype=torch.float32, device=self.device).reshape(1, nsteps_test, 1) * 10
-        batMax = torch.tensor(np.ones((1,nsteps_test+1,1))*8.0, dtype=torch.float32, device=self.device)
+        stored0 = torch.tensor(self.rng.uniform(0,self.batSize,[1,1,1]), dtype=torch.float32, device=self.device)
+        batRefYear = np.tile(pd.read_csv('socSchedule.csv', header=None).to_numpy(), (len(dataset['D']) // 1440,1)) * self.batSize
+        batRef = torch.tensor(self.Get_D(batRefYear, nsteps_test, start_idx), dtype=torch.float32, device=self.device).unsqueeze(0)
+        batMax = torch.tensor(np.ones((1,nsteps_test+1,1))*0.8*self.batSize, dtype=torch.float32, device=self.device)
 
         data = {'xn': x0,
                 'y': x0[:,:,self.y_idx],

community_sim/communityController/communityController.py
@@ -67,8 +67,10 @@ class CommunityController:
         # Forecast flexibility
 
         # Update coordinator
-        coordinateSignals = {}
-        self.coordinator.Step(self.predictedLoad, self.predictedFlex)
+        self.coordinator.predictedLoad = self.predictedLoad
+        self.coordinator.predictedFlexibility = self.predictedFlex
+        self.coordinator.baseLoad = np.ones(self.coordinator.nsteps) * 10
+        self.coordinator.Step()
         coordinateSignals = self.coordinator.reductionFactor
 
         # Update controllers

community_sim/communityController/coordinator/community_optim.py
@@ -19,6 +19,7 @@ class Coordinator():
         self.reductionFactor = np.zeros((self.nsteps, self.numBuildings))
         self.predictedLoad = np.zeros((self.nsteps, self.numBuildings))
         self.predictedFlexibility = np.zeros((self.nsteps, self.numBuildings))
+        self.baseLoad = np.zeros((self.nsteps))
         self.countSinceChange = 0
 
         self.dirName = os.path.dirname(__file__)
@@ -78,7 +79,7 @@ class Coordinator():
             self.reductionFactor[:10,:] = np.ones((10, self.numBuildings)) * np.random.randint(0, 2)
             self.countSinceChange = 0
 
-    def Step(self, predictedLoad, predictedFlex):
+    def Step(self):
         '''
         '''
         if self.Assess():